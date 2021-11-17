1a67a04e8fc115a8e5471ce38e9a53fd1a4121ba
Maurice Collier <mauricecollier84@yahoo.com>
2021-11-17 15:43:06 -0500

    Created react-events-state-exercise with the bonus files.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+// 11/17/21 react events state exercise
+// 1d
+import Bonus from "./bonus/Bonus";
+import Exercise from "./exercise/Exercise";
+// 3g
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* 1d */}
+    <Exercise />
+    {/* 3h */}
+    <Bonus />
     </div>
   );
 }
